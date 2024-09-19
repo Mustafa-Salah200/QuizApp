91ae674498ee9be4d9b2d377bb88c858ca2e50e4
Mustafa-Salah200 <mustafaslahtoofy@gmail.com>
2024-09-19 22:11:16 +0200

    update some style

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./poppins.css";
 import { ProtectApp } from "./App";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./rtk/store";
 import Signin from "./component/Signin";
@@ -14,7 +14,7 @@ FirebasrApp.init();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <Routes>
           <Route path="/signin" element={<Signin />} />
@@ -22,6 +22,6 @@ root.render(
           <Route path="*" element={<ProtectApp />} />
         </Routes>
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
